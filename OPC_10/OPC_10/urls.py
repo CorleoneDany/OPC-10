62c349a0255bab51857b72a57e09e9c34f3aa026
Dany Corleone <corleonedany@live.fr>
2022-03-11 09:51:28 +0100

    routeurs fonctionnels via la librairie nested routers

--- OPC_10/OPC_10/urls.py
+++ OPC_10/OPC_10/urls.py
@@ -13,10 +13,10 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
-from django import views
 from django.contrib import admin
 from django.urls import path, include
-from rest_framework import routers
+from rest_framework_nested import routers
+
 from SoftDesk.views import ContributorViewSet, IssueViewSet, ProjectViewSet, UserViewSet, CommentViewSet
 from rest_framework_simplejwt.views import (
     TokenObtainPairView,
@@ -24,18 +24,26 @@ from rest_framework_simplejwt.views import (
 )
 
 router = routers.SimpleRouter()
-router.register('user', UserViewSet)
-router.register('projects', ProjectViewSet)
-router.register('projects/{project_id}/users', ContributorViewSet)
-router.register('projects/{project_id}/issues', IssueViewSet)
-router.register(
-    'projects/{project_id}/issues/{issue_id}/comments', CommentViewSet)
+router.register('user', UserViewSet, 'user')
+router.register('projects', ProjectViewSet, 'projects')
+
+
+project_router = routers.NestedSimpleRouter(
+    router, 'projects', lookup='project')
+project_router.register('contributors', ContributorViewSet, 'contributors')
+project_router.register('issues', IssueViewSet, 'issues')
+
+
+issues_router = routers.NestedSimpleRouter(
+    project_router, 'issues', lookup='issue')
+issues_router.register('comments', CommentViewSet, 'comments')
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api-auth/', include('rest_framework.urls')),
     path('api/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
     path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
+    path('', include(router.urls)),
+    path('', include(project_router.urls)),
+    path('', include(issues_router.urls)),
 ]
-
-urlpatterns += router.urls
